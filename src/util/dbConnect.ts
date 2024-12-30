a778ea9175ed416410160827e5b91366d6e288cc
ashwinhegde19 <ashwinhegde19@gmail.com>
2024-12-30 14:57:35 +0530

    add database connection

---
+++ src/util/dbConnect.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+    isConnected?: number;
+};
+
+const connection: ConnectionObject = {}
+
+async function dbConnect(): Promise<void> {
+    if (connection.isConnected) {
+        console.log("Already connected to Database");
+        return
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI || '', {})
+        connection.isConnected = db.connections[0].readyState;
+        console.log("DB",db);
+        console.log("Connections",db.connections);
+        
+        
+        console.log("Connected to Database");
+
+    } catch (error) {
+        console.log("Error connecting to Database", error);
+        process.exit(1);
+    }
+}
+
+export default dbConnect;

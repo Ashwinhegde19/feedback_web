d9d10c7953c795d1aa2a06c87198e15f2e1ac832
ashwinhegde19 <ashwinhegde19@gmail.com>
2025-01-01 00:05:40 +0530

    add additional user properties to JWT and session

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -46,10 +46,21 @@ export default {
         async jwt(token, user) {
             if (user) {
                 token._id = user._id?.toString()
+                token.username = user.username
+                token.isVerified = user.isVerified
+                token.isAcceptingMessages = user.isAcceptingMessages
             }
+
             return token
         },
         async session(session, token) {
+            if(token) {
+                session.user._id = token._id
+                session.user.username = token.username
+                session.user.isVerified = token.isVerified
+                session.user.isAcceptingMessages = token.isAcceptingMessages
+            }
+
             return session
         }
     }
